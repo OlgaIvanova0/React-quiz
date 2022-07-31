04cc0f3f85d27c73f8f5f0492134b098932e0dd2
OlgaIvanova0 <88612307+OlgaIvanova0@users.noreply.github.com>
2022-07-31 18:51:45 +0500

    ok

--- react-quiz/src/components/Auth/Auth.js
+++ react-quiz/src/components/Auth/Auth.js
@@ -1,10 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import style from './Auth.module.css';
+import Button from '../UI/Button';
+import Input from '../UI/Input/Input';
 
 export class Auth extends Component {
+
+  loginHandler = () =>{}
+
+  registrHandler = () =>{}
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={style.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form onSubmit={this.submitHandler} className={style.AuthForm}>
+            <Input label='Email' />
+            <Input label='Пароль' errorMessage={'test'}/>
+            <Button type='Btn-success'
+            onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type='Btn-primary'
+            onClick={this.registrHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
